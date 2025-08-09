cc47686652e3dba33096eaa82ed88af7f750484a
Alex Crist <accrist@gmail.com>
2025-08-09 14:07:51 -0700

    working must-schedule constraint

--- main.py
+++ main.py
@@ -32,5 +32,41 @@ scheduled = model.NewBoolVarSeries(
     pd.Index((list(it.product(residents.index, rotations.index, weeks.index)))),
 ).sort_index()
 
-ic(scheduled)
+# TODO Constraints
+# every resident's week x must have exactly 1 scheduled rotation or vacation (which is a "rotation")
+for resident, week in it.product(residents.index, weeks.index):
+    model.AddExactlyOne(scheduled.loc[pd.IndexSlice[resident, :, week]])
 
+
+ic(model.ModelStats())
+
+# solve model
+solver = cp_model.CpSolver()
+solver.parameters.log_search_progress = True
+solver.log_callback = print
+status = solver.Solve(model)
+
+if status == cp_model.OPTIMAL:
+    print("optimal")
+    print("=====Stats:======")
+    print(solver.SolutionInfo())
+    print(solver.ResponseStats())
+elif status == cp_model.FEASIBLE:
+    print("feasible")
+    print("=====Stats:======")
+    print(solver.SolutionInfo())
+    print(solver.ResponseStats())
+else:
+    print("no solution")
+
+print_full_DataFrame(
+    solver.Values(scheduled)[solver.Values(scheduled) == 1]
+    .sort_index(level=(0, 2))
+    .unstack()
+    .reset_index()
+    .melt(id_vars=["level_0", "level_1"])
+    .query("value == 1")
+    .set_index(["level_0", "variable"])
+    .sort_index()["level_1"]
+    .unstack()
+)
