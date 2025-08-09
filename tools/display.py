48d01feadb7de040b873254ff8bba437c3f64525
Alex Crist <accrist@gmail.com>
2025-08-08 19:08:06 -0700

    add display tool module

---
+++ tools/display.py
@@ -0,0 +1,11 @@
+def print_full_DataFrame(df):
+    """
+    Prints a DataFrame in full, overriding truncation, then resets the option.
+    Source: https://stackoverflow.com/questions/2058552/how-to-print-a-pandas-data
+    """
+    import pandas as pd
+
+    pd.set_option("display.max_rows", len(df))
+    print(df)
+    pd.reset_option("display.max_rows")
+    return None
