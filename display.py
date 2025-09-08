dc3b11f23a144579e338371629e8749fdaaebab6
Alex Crist <accrist@gmail.com>
2025-09-08 12:18:38 -0700

    Update display.py
    Todo for catching display failures

--- display.py
+++ display.py
@@ -40,7 +40,7 @@ def convert_melted_to_block_schedule(solved_schedule: pl.DataFrame) -> pl.DataFr
     Returns:
         block_schedule: pivoted df
     """
-
+    #TODO should catch unrenderable/not 1:1 before Polars fails ugly on it
     renderable_df = solved_schedule.select(pl.all().exclude(cpmpy_variable_column))
     filtered_long_format = renderable_df.filter(pl.col(cpmpy_result_column))
     block_schedule = filtered_long_format.pivot(
