9b8f94284a806b5355fd6d6de36e58df42094bb5
Alex Crist <accrist@gmail.com>
2025-09-11 19:04:39 -0700

    parameterize testing_db for input reader

--- test_data_io.py
+++ test_data_io.py
@@ -14,8 +14,7 @@ tester_weeks = pl.read_csv(config.testing_files.weeks.tiny, try_parse_dates=True
 
 
 def test_read_bulk_data_sqlite3():
-    test_read_tables = read_bulk_data_sqlite3("residency_mastermind.db")
-
+    test_read_tables = read_bulk_data_sqlite3(config.testing_db_path)
     assert isinstance(test_read_tables["residents"], pl.DataFrame)
 
     dtype_of_weeks_monday_date = (
@@ -31,4 +30,3 @@ def test_generate_pl_wrapped_boolvar():
         tester_residents, tester_rotations, tester_weeks
     )
     assert isinstance(fake_scheduled, pl.DataFrame)
-    assert len(fake_scheduled) == 54
