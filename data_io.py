494c85f00d00c432962ef2fc32d023dd202b9708
Alex Crist <accrist@gmail.com>
2026-09-05 12:48:35 -0700

    working req set generation

--- data_io.py
+++ data_io.py
@@ -1,10 +1,11 @@
 import cpmpy as cp
+import mergedeep
 import polars as pl
 import yaml
 
 
 def generate_pl_wrapped_boolvar(
-    workers: pl.DataFrame, rotations: dict[str,dict], weeks: pl.DataFrame
+    workers: pl.DataFrame, rotations: dict[str, dict], weeks: pl.DataFrame
 ) -> pl.DataFrame:
     """
     Generate a Polars DataFrame wrapper around 3D CP-SAT boolean variables.
@@ -18,7 +19,7 @@ def generate_pl_wrapped_boolvar(
         pl.DataFrame `scheduled`: wrapped around 3D array of workers, rotations, weeks
         for ease of complex indexing by string variables.
     """
-    rotations_df = pl.DataFrame({"rotation":rotations.keys()})
+    rotations_df = pl.DataFrame({"rotation": rotations.keys()})
 
     combinations = workers.join(rotations_df, how="cross").join(weeks, how="cross")
 
@@ -37,6 +38,74 @@ def generate_pl_wrapped_boolvar(
     )
 
     return scheduled
+
+
+def compose_requirements_to_workers(
+    workers: pl.DataFrame, requirement_sets: dict[str, dict]
+) -> pl.DataFrame:
+    workers_reqs = {}
+
+    for worker in workers.iter_rows(named=True):
+        this_workers_reqs = {}
+        match worker["year"]:
+            case "R2":
+                this_workers_reqs.update(requirement_sets.get("R2 Base", {}))
+                match worker["track"]:
+                    case "PCT":
+                        this_workers_reqs = mergedeep.merge(
+                            this_workers_reqs,
+                            requirement_sets["R2 PCT"],
+                            strategy=mergedeep.Strategy.REPLACE,
+                        )
+                    case "Fellowship":
+                        this_workers_reqs = mergedeep.merge(
+                            this_workers_reqs,
+                            requirement_sets.get("R2 Fellowship", {}),
+                            strategy=mergedeep.Strategy.REPLACE,
+                        )
+                    case "Standard":
+                        this_workers_reqs = mergedeep.merge(
+                            this_workers_reqs,
+                            requirement_sets.get("R2 Standard", {}),
+                            strategy=mergedeep.Strategy.REPLACE,
+                        )
+                    case _:
+                        raise RuntimeError(f"{worker['track']=} doesn't exist")
+            case "R3":
+                this_workers_reqs.update(requirement_sets.get("R3 Base", {}))
+                match worker["track"]:
+                    case "PCT":
+                        this_workers_reqs = mergedeep.merge(
+                            this_workers_reqs,
+                            requirement_sets["R3 PCT"],
+                            strategy=mergedeep.Strategy.REPLACE,
+                        )
+                    case "Fellowship":
+                        this_workers_reqs = mergedeep.merge(
+                            this_workers_reqs,
+                            requirement_sets.get("R3 Fellowship", {}),
+                            strategy=mergedeep.Strategy.REPLACE,
+                        )
+                    case "Standard":
+                        this_workers_reqs = mergedeep.merge(
+                            this_workers_reqs,
+                            requirement_sets.get("R3 Standard", {}),
+                            strategy=mergedeep.Strategy.REPLACE,
+                        )
+                    case _:
+                        raise NotImplementedError(f"{worker['track']=} doesn't exist")
+            case "R1":
+                raise NotImplementedError("Don't plan to implement R1")
+            case _:
+                raise RuntimeError(f"{worker['year']=} doesn't exist")
+
+        workers_reqs.update({worker["name"]: this_workers_reqs})
+
+    workers_df_with_reqs = pl.DataFrame(
+        {"name": workers_reqs.keys(), "req_set": workers_reqs.values()}
+    ).join(workers, on="name")
+
+    return workers_df_with_reqs
 
 
 def dump_polars_df_to_yaml(df: pl.DataFrame) -> str:
--- test_data_io.py
+++ test_data_io.py
@@ -1,10 +1,11 @@
 import sys
 
+import mergedeep
 import polars as pl
 import pytest
 from loguru import logger
 
-from data_io import generate_pl_wrapped_boolvar
+from data_io import compose_requirements_to_workers, generate_pl_wrapped_boolvar
 
 logger.add(
     sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO"
@@ -12,11 +13,40 @@ logger.add(
 
 
 @pytest.fixture
+def minimal_req_composition() -> dict[str, dict]:
+    req_set = {
+        "R2 Base": {
+            "HS Rounding Senior": {
+                "constraints": {"min_weeks": 0, "max_weeks": 4},
+                "fulfilled_by": [
+                    "Green HS Senior",
+                    "Orange HS Senior",
+                ],
+            },
+        },
+        "R2 PCT": {
+            "ICU Senior": {
+                "constraints": {"min_weeks": 0, "max_weeks": 4},
+                "fulfilled_by": ["SHMC ICU Senior"],
+            }
+        },
+        "R2 Fellowship": {
+            "ICU Senior": {
+                "constraints": {"min_weeks": 4, "max_weeks": 4},
+                "fulfilled_by": ["SHMC ICU Senior"],
+            }
+        },
+    }
+
+    return req_set
+
+
+@pytest.fixture
 def minimal_case_setup():
     workers = pl.DataFrame(
         [
-            {"name": "Aaron Aaronson", "year": "R1", "track": "Standard"},
-            {"name": "Bill Byornsen", "year": "R1", "track": "PCT"},
+            {"name": "Aaron Aaronson", "year": "R2", "track": "Standard"},
+            {"name": "Bill Byornsen", "year": "R2", "track": "PCT"},
         ]
     )
     rotations = {
@@ -240,10 +270,45 @@ def large_case_setup():
 
 
 def test_generate_pl_wrapped_boolvar(minimal_case_setup):
-
     workers, rotations, weeks, _requirements = minimal_case_setup
 
     wrapped = generate_pl_wrapped_boolvar(workers, rotations, weeks)
 
     assert isinstance(wrapped, pl.DataFrame)
     assert wrapped.shape[0] == len(workers) * len(rotations) * len(weeks)
+
+
+def test_compose_requirements_to_workers(minimal_case_setup, minimal_req_composition):
+    workers, _rotations, _weeks, _requirements = minimal_case_setup
+    req_sets = minimal_req_composition
+
+    workers_with_reqs = compose_requirements_to_workers(workers, req_sets)
+
+    target_raw_data = pl.DataFrame(
+        [
+            {
+                "name": "Aaron Aaronson",
+                "req_set": {
+                    "HS Rounding Senior": {
+                        "constraints": {"min_weeks": 0, "max_weeks": 4},
+                        "fulfilled_by": ["Green HS Senior", "Orange HS Senior"],
+                    }
+                },
+                "year": "R2",
+                "track": "Standard",
+            },
+            {
+                "name": "Bill Byornsen",
+                "req_set": {
+                    "HS Rounding Senior": {
+                        "constraints": {"min_weeks": 0, "max_weeks": 4},
+                        "fulfilled_by": ["Green HS Senior", "Orange HS Senior"],
+                    }
+                },
+                "year": "R2",
+                "track": "PCT",
+            },
+        ]
+    )
+
+    assert workers_with_reqs.equals(target_raw_data)
